69d9c511c10141e02e0ed6afc9918b1f1f3f24eb
threadexio <pzarganitis@gmail.com>
2025-09-21 16:40:38 +0300

    update docs

--- magic-args/src/lib.rs
+++ magic-args/src/lib.rs
@@ -1,17 +1,16 @@
 //! "Magic" declarative-style function arguments.
 //!
+//! A cursed exercise of the type system.
+//!
 //! ```
-//! # use magic_args::{apply, MagicArgs};
+//! # use magic_args::apply;
 //! fn f0() { /* ... */ }
 //! fn f1(x: i32) { /* ... */ }
 //! fn f2(x: i32, z: usize) { /* ... */ }
 //! async fn f3(y: &'static str) { /* ... */ }
 //! async fn f4(y: &'static str, x: i32, z: usize) { /* ... */ }
 //!
-//! #[derive(MagicArgs)]
-//! struct Args(i32, &'static str, usize);
-//!
-//! let args = Args(-42, "🦀", 42);
+//! let args = (-42_i32, "🦀", 42_usize);
 //!
 //! let _ = apply(f0, &args);
 //! let _ = apply(f1, &args);
@@ -25,75 +24,197 @@
 //!
 //! # Quick start
 //!
-//! 1. You define a "set of arguments".
+//! ```
+//! use magic_args::apply;
 //!
-//! ```no_run
-//! # use magic_args::MagicArgs;
+//! fn f(x: usize, z: &str) -> usize { x + z.len() }
+//!
+//! let args = (31_i32, "foo", 42_usize);
+//!
+//! let y = apply(f, args);
+//! assert_eq!(y, 45);
+//! ```
+//!
+//! It is also possible to have a custom type as `args` (instead of a tuple).
+//!
+//! ```
+//! use magic_args::{apply, MagicArgs};
+//!
+//! fn f(x: usize, z: &str) -> usize { x + z.len() }
+//!
 //! #[derive(MagicArgs)]
-//! pub struct MyArgs(&'static str, usize /*, ... */);
+//! struct MyArgs(i32, &'static str, usize);
 //!
-//! let args = MyArgs("Hello, world!", 42);
+//! let args = MyArgs(31, "foo", 42);
+//!
+//! let y = apply(f, args);
+//! assert_eq!(y, 45);
 //! ```
-//!
-//! 2. You use [`apply`] to call a function of your choosing.
-//!
-//! ```no_run
-//! # use magic_args::{apply, MagicArgs};
-//! # #[derive(MagicArgs)]
-//! # pub struct MyArgs(&'static str, usize /*, ... */);
-//! # let args = MyArgs("Hello, world!", 42);
-//! fn my_func(msg: &'static str) { /* ... */ }
-//!
-//! let result = apply(my_func, args);
-//! ```
-//!
-//! 3. Profit? 💰
-//!
-//!
-//!
-//! <sub>*this product contains limitations which are not featured above</sub>
 //!
 //! # How it works
 //!
-//! All of the "magic" of this crate is done via [`Callable`] and [`Args`].
-//! Everything else is purely cosmetic and for convinience.
+//! The core of this crate is [`Callable`] and [`Args`]. Everything else is only
+//! for convenience and ease of use.
 //!
-//! This documentation will use the term "argument set" to refer to any type $T$
-//! which implements `Args<U>` for some $U$.
+//! I will now try to _briefly_ explain how this crate works.
 //!
-//! [`Callable`] is defined in terms of $A$ and $T$. $A$ is the argument set
-//! which contains all arguments available to the function. $T$ is an ordered
-//! tuple containing the arguments the function expects to be given.
-//! [`Callable`] is implemented automatically for all `FnOnce(T0, T1, ...) -> O`
-//! (up to 32 arguments). The job of the [`Callable`] trait is to pick out what
-//! arguments the function wants from a given $A$. For this to be possible, $A$
-//! must be bounded such that $T \subseteq A$. In other words; to be able to
-//! even call the function with the arguments it expects, we must _have_ the
-//! arguments in the first place. For example, let $T = (T_0, T_1, T_2)$. It
-//! must be that $T_0, T_1, T_2 \in A$ or, in code,
-//! `A: Args<T0> + Args<T1> + Args<T2>`. Picking the correct argument from the
-//! argument set $A$ for each argument of the function is done with
-//! [`Args::get`] and the compiler's type inference.
+//! ## The [`Args`] trait
+//!
+//! The [`Args`] trait describes any kind of type that can act as an "argument
+//! set". This is essentially the type which contains every argument available.
+//!
+//! The [`Args`] trait has blanket implementations for tuples of up to 32
+//! elements. It is also possible to turn any type into [`Args`] with the
+//! [`macro@MagicArgs`] macro. It _is_ possible to hand-implement [`Args`] but
+//! this is not recommended as you must rely on the internal constructs of this
+//! crate that are subject to change at any time.
+//!
+//! ## The [`Callable`] trait
+//!
+//! This is where the magic happens. [`Callable`] describes anything that can
+//! be called with an argument set. It is blanket-implemented for any `FnOnce`
+//! with up to 32 arguments.
+//!
+//! The trait is defined over $A$ and $T$. $A$ is the argument set needed and
+//! $T$ is an ordered tuple which contains the types of the arguments the
+//! function expects to receive. For example, in the following function:
+//!
+//! ```no_run
+//! fn f(x: u32, y: i32, z: usize) {}
+//! ```
+//!
+//! $A$ is any `A: Args<u32> + Args<i32> + Args<usize>` and $T$ is `(u32, i32,
+//! usize)`. The type parameter $T$ is only there to provide disambiguation
+//! for the different `impl`s. Without it, it would be impossible to provide
+//! implementations of `Callable<A>` for `FnOnce()` and `FnOnce(U)` at the same
+//! time. This is because the language, at this time, lacks [specialized trait
+//! `impl`s]. With $T$, the implemented trait is `Callable<A, ()>` for `FnOnce()`
+//! and `Callable<A, (U,)>` for `FnOnce(U)` (which are technically different
+//! traits and thus are allowed to coexist with blanket implementations).
+//!
+//! ## An implementation detail
+//!
+//! **NOTE:** This section serves only as a guide for hackers and just
+//! to explain how the crate works. Nothing here is to be considered as a
+//! semver-stable API. Consider yourself warned.
+//!
+//! If you tried to implement [`Args`] for a tuple of `(T0, T1)`, it would
+//! probably look a bit like this:
+//!
+//! ```compile_fail,E0119
+//! # use magic_args::Args;
+//! impl<T0: Clone, T1> Args<T0> for (T0, T1) {
+//!     fn get(&self) -> T0 { self.0.clone() }
+//! }
+//!
+//! impl<T0, T1: Clone> Args<T1> for (T0, T1) {
+//!     fn get(&self) -> T1 { self.1.clone() }
+//! }
+//! ```
+//!
+//! Which, as you might have guessed from the red border above, does not work.
+//! This is because the above implementations are not well-defined. Consider the
+//! following type, $(i32, i32)$. What happens when we invoke [`Args::get`] on
+//! that type? Do we get back the first or the second field? This is the edge
+//! case the compiler tries to warn us about. As of writing this crate, there is
+//! no support for specifying type bounds like `T0 != T1`. So we can't just say
+//! "where `T0 != T1`" and be done.
+//!
+//! There is another, arguably more convuluted way to describe this. If we
+//! introduce a type `Tagged<T, const N: usize>(T)` we can have many "unique"
+//! $T$s. This is because `Tagged<i32, 0>` is not the same as `Tagged<i32, 1>`.
+//! We can use this little property to modify our [`Args`] implementation a bit;
+//! instead of returning `T0` or `T1`, we return `Tagged<T0, 0>` and
+//! `Tagged<T1, 1>` respectively. This solves our previous issue of
+//! "conflicting implementations" since we are now implementing what is now
+//! 2 different traits; `Args<Tagged<T0, 0>>` and `Args<Tagged<T1, 1>>`. We can
+//! now modify the [`Callable`] `impl`s to take `const N0: usize`,
+//! `const N1: usize` and `A: Args<Tagged<T0, N0>> + Args<Tagged<T1, N1>>`.
+//! This solves our issue and allows all implementations to coexist. For tuples,
+//! the `N` constant in `Tagged` is the index of the field. The
+//! [`macro@MagicArgs`] also uses the index of the field for `N`. `N` is there
+//! to serve as a "tag" for each field. Its value does not really matter, only
+//! that it is different for each field.
 //!
 //! # Limitations
 //!
-//! This whole crates operates on the type-level of arguments. For this reason,
-//! it is impossible to have a function which accepts 2 different instances of
-//! the same type. To see why this is, consider the following function:
+//! - This crate operates wholly at the type-level. There is no runtime code
+//!   generated as part of resolving arguments, etc. This makes the crate very
+//!   difficult to use in a dynamic setting.
 //!
-//! ```no_run
+//! - You cannot have 2 different instances of the same type as arguments. For
+//!   example:
+//!
+//! ```compile_fail,E0284
+//! # use magic_args::apply;
+//! fn f(x: i32, y: i32) -> i32 { x + y }
+//!
+//! let args: (i32, i32) = (42, 31);
+//! let _y: i32 = apply(f, args);
+//! ```
+//!
+//!   This can be explained with the following example. Consider the following
+//!   function:
+//!
+//! ```
 //! fn f(x: i32, _y: i32) -> i32 { x }
 //! ```
 //!
-//! Given its signature $f: (i32, i32) \to i32$ and an arguments set
-//! $A = \\{ 42, -42 \\}$. It is impossible to know the correct order of passing
-//! these values to $f$ such that it returns 42, or -42 for that matter.
-//! This means all values in the argument set must be of a different type. In
-//! cases like above when a function has 2 arguments of the same type, the
-//! value of the matching type in the argument set is passed twice. This incurs
-//! a [`Clone::clone`] call.
+//!   Given only the signature of the function, $f: (i32, i32) \to i32$, can you
+//!   figure out the correct order to pass the values $42$ and $31$ such that
+//!   $f$ returns $42$? Spoiler alert: No. It is impossible. In this case, it is
+//!   not clear how we should pass the arguments to the function. Trying the
+//!   above will result in a cryptic error message(s). This can be alleviated
+//!   however by using a thin wrapper type which semantically conveys the
+//!   meaning of the data.
 //!
+//! ```
+//! # use magic_args::apply;
+//! #[derive(Clone)]
+//! struct X(pub i32);
+//!
+//! #[derive(Clone)]
+//! struct Y(pub i32);
+//!
+//! fn f(X(x): X, Y(y): Y) -> i32 { x + y }
+//!
+//! let args = (X(42), Y(31));
+//! let y = apply(f, args);
+//! assert_eq!(y, 73);
+//! ```
+//!
+//! - Passing non-[`Clone`] arguments is not ideal. Arguments need to be
+//!   [`Clone`] so the following is well-defined:
+//!
+//! ```
+//! # use magic_args::apply;
+//! fn f(x: i32, y: i32) -> i32 { x + y }
+//!
+//! let args = (42,);
+//! let y = apply(f, args);
+//! assert_eq!(y, 84)
+//! ```
+//!
+//!   Notice how this is different than the example before; we are only passing
+//!   one [`i32`], not two so there is no ambiguity here. In this case, the
+//!   value of [`i32`] is [`Clone::clone`]d and passed both as `x` and as `y`.
+//!   Meaning `f` could be rewritten as:
+//!
+//! ```
+//! fn f(x: i32) -> i32 { x * 2 }
+//! ```
+//!
+//!   It is possible to pass non-[`Clone`] arguments, but that needs runtime
+//!   checking to ensure only one instance exists. This can be done with
+//!   [`std::cell::RefCell`] if necessary.
+//!
+//! ---
+//!
+//! Enjoy responsibly!
+//!
+//! [specialized trait `impl`s]: https://github.com/rust-lang/rust/issues/31844
 //! [axum's route handlers]: https://docs.rs/axum/latest/axum/handler/index.html
+//! [`std::cell::RefCell`]: https://doc.rust-lang.org/stable/std/cell/struct.RefCell.html
 
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]
 #![forbid(unsafe_code)]
@@ -284,6 +405,16 @@ pub trait MagicArgs {
     /// Equivalent to: `apply(f, self)`.
     ///
     /// See: [`apply`].
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use magic_args::{apply, MagicArgs};
+    /// fn f(x: i32) -> i32 { x + 1 }
+    ///
+    /// let y = ("🦀", 41).apply(f);
+    /// assert_eq!(y, 42);
+    /// ```
     fn apply<C, T>(self, f: C) -> C::Output
     where
         C: Callable<Self, T>,
@@ -311,40 +442,10 @@ impl<U> MagicArgs for U {
 ///
 /// ```
 /// # use magic_args::{apply, MagicArgs};
-/// #[derive(MagicArgs)]
-/// struct Args(String, i32);
-///
 /// fn f(x: i32) -> i32 { x + 1 }
 ///
-/// let args = Args("🦀".into(), 41);
-/// let y = apply(f, args);
+/// let y = apply(f, ("🦀", 41));
 /// assert_eq!(y, 42);
-/// ```
-///
-/// ---
-///
-/// ```
-/// # use magic_args::{apply, MagicArgs};
-/// #[derive(MagicArgs)]
-/// struct Args(String, i32);
-///
-/// let args = Args("🦀".into(), 41);
-///
-/// let fs: [fn(i32) -> i32; _] = [
-///     (|x| x + 1),
-///     (|x| x * 2),
-///     (|x| 3 * x + 1),
-///     (|x| -x),
-/// ];
-///
-/// let results = fs.map(|f| apply(f, &args));
-///
-/// assert_eq!(results, [
-///     42,
-///     82,
-///     124,
-///     -41
-/// ]);
 /// ```
 #[inline]
 pub fn apply<C, A, T>(f: C, args: A) -> C::Output
